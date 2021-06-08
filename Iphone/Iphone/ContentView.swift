d4f19a34250689ff50ff0eac2d0b310cc42266e9
Naoya <nanetto777@gmail.com>
2021-06-08 22:26:39 +0900

    Swift追加

---
+++ Iphone/Iphone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Iphone
+//
+//  Created by matuda naoya on 2021/06/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
