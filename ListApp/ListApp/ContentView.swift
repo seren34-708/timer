7d0fc01607ba5f8f01655da6bcbe54ebe7ee87ea
Naoya <nanetto777@gmail.com>
2021-06-16 21:03:26 +0900

    chapter4

--- ListApp/ListApp/ContentView.swift
+++ ListApp/ListApp/ContentView.swift
@@ -11,13 +11,15 @@ struct ContentView: View {
     var body: some View {
             NavigationView {
                 List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
-                    NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Navigate")/*@END_MENU_TOKEN@*/} }.navigationTitle("Navigation Bar")
+                    NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Navigate")/*@END_MENU_TOKEN@*/} }.navigationTitle("リスト")
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
