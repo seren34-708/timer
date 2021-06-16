88fd29bc2d2d34f2e6414d39ad34aac758b2ebfa
seren34-708 <nanetto777@gmail.com>
2021-06-16 21:10:25 +0900

    Issue/5 (#2)
    * iphoneアプリのHelle word
    
    * Listを作成
    
    * chapter4

---
+++ ListApp/ListApp/ListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListAppApp.swift
+//  ListApp
+//
+//  Created by matuda naoya on 2021/06/11.
+//
+
+import SwiftUI
+
+@main
+struct ListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
