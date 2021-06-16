cf647d4ebdf57305a1f33310c6d817d92350de28
Naoya <nanetto777@gmail.com>
2021-06-16 21:40:19 +0900

    STEP1のプロジェクトを追加

---
+++ HelloApp/HelloApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloApp
+//
+//  Created by matuda naoya on 2021/06/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloApp/HelloApp/HelloAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloAppApp.swift
+//  HelloApp
+//
+//  Created by matuda naoya on 2021/06/16.
+//
+
+import SwiftUI
+
+@main
+struct HelloAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
